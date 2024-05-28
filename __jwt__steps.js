500d3dcccc64cc9686495985d9e69e3db82175f2
Meherab Hosssain Bhuiyan <127032892+Meherab003@users.noreply.github.com>
2024-05-28 21:16:25 +0600

    Module 60

---
+++ __jwt__steps.js
@@ -0,0 +1,25 @@
+/**
+ * install jsonwebtoken
+ * jwt.sign (payload, secret, {expiresIn:})
+ * token client
+ */
+
+/**
+ * how to store token in the client site
+ * 1. memory => ok type;
+ * 2. local storage => ok type (XSS)
+ * 3. cookies => http only 
+ */
+
+/**
+ * 1. set cookies with http only. for development secure: false,
+ * 
+ * 2. cors
+ * app.use(cors({
+        origin: ['http://localhost:5173'],
+        credentials: true,
+    }));
+ * 
+ * 3. client side axios settings
+ * in axios set withCredentials: true
+ */
--- index.js
+++ index.js
@@ -1,12 +1,18 @@
 const express = require('express');
 const cors = require('cors');
+const jwt = require("jsonwebtoken");
+const cookieParser = require("cookie-parser");
 require('dotenv').config();
 const app = express();
 const port = process.env.PORT || 5000;
 
 //Middleware
-app.use(cors());
+app.use(cors({
+  origin: ['http://localhost:5173'],
+  credentials: true,
+}));
 app.use(express.json());
+app.use(cookieParser());
 
 //MongoDB
 
@@ -22,6 +28,31 @@ const client = new MongoClient(uri, {
   }
 });
 
+//custom middlewares
+const logger = async (req, res, next) => {
+  console.log("called:", req.host, req.originalUrl);
+  next()
+}
+
+const verifyToken = async (req, res, next) => {
+  const token = req.cookies?.token;
+  console.log("value of token in middleware", token)
+  if (!token) {
+    return res.status(401).send({ message: "not authorized" })
+  }
+  jwt.verify(token, process.env.ACCESS_TOKEN_SECRET, (err, decoded) => {
+    //err
+    if (err) {
+      console.log(err)
+      return res.status(401).send({ message: "unauthorized" })
+    }
+    //if token is valid then it would be decoded
+    console.log("value in the token", decoded)
+    req.user = decoded;
+    next()
+  })
+}
+
 async function run() {
   try {
     // Connect the client to the server	(optional starting in v4.7)
@@ -33,7 +64,25 @@ async function run() {
 
 
     //Mongo CRUD operations
-    app.get("/services", async (req, res) => {
+
+
+    //Auth related apis
+    app.post('/jwt', logger, async (req, res) => {
+      const user = req.body;
+      console.log(user);
+
+      const token = jwt.sign(user, process.env.ACCESS_TOKEN_SECRET, { expiresIn: '1h' });
+
+      res
+        .cookie("token", token, {
+          httpOnly: true,
+          secure: false,
+        })
+        .send({ success: true })
+    })
+
+    //Services
+    app.get("/services", logger, async (req, res) => {
       const cursor = servicesCollection.find();
       const result = await cursor.toArray();
       res.send(result);
@@ -52,15 +101,13 @@ async function run() {
     })
 
     //Booking CRUD
-    app.post('/bookings', async (req, res) => {
-      const booking = req.body;
-      // console.log(booking)
-      const result = await bookingCollection.insertOne(booking);
-      res.send(result);
-    })
-
-    app.get("/bookings", async (req, res) => {
+    app.get("/bookings", logger, verifyToken, async (req, res) => {
       console.log(req.query.email);
+      // console.log('tttt', req.cookies)
+      console.log("user in the valid token: ", req.user)
+      if(req.query.email !== req.user.email){
+        return res.status(403).send({message: "forbidden access"})
+      }
       let query = {};
       if (req.query?.email) {
         query = { email: req.query.email }
@@ -69,13 +116,20 @@ async function run() {
       res.send(result)
     })
 
+    app.post('/bookings', async (req, res) => {
+      const booking = req.body;
+      // console.log(booking)
+      const result = await bookingCollection.insertOne(booking);
+      res.send(result);
+    })
+
     app.patch('/bookings/:id', async (req, res) => {
       const id = req.params.id;
       const filter = { _id: new ObjectId(id) };
       const updateBooking = req.body;
       const updatedDoc = {
         $set: {
-          status: updateBooking.status,
+          status: updateBooking.status
         }
       }
       // console.log(updateBooking)
